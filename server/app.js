cc2f6c614e2d600757d0e4d217d317f31393842f
Rosemary Ku <rosemary.ku@hotmail.com>
2022-03-18 14:17:04 -0400

    connect to back end

--- server/app.js
+++ server/app.js
@@ -1,27 +1,30 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("dotenv").config();
+const cors = require("cors");
 
-const db = require('./configs/db.config'); //<--------- ADD
+// const { PORT, ENVIORONMENT } = process.env;
 
-// ---  test of app.js.................
+const db = require("./configs/db.config");
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// require routes
+var indexRoutes = require("./routes/index");
+var usersRoutes = require("./routes/users");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(cors({ origin: ["http://localhost:3000"] }));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter(db));
-
+app.use("/", indexRoutes);
+app.use("/users", usersRoutes(db));
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,39 @@
+// // -- routes/users.js
+// const router = require("express").Router();
+
+// // mock data
+// const data = [
+//   {
+//     name: "Edwin",
+//     hobby: "skateboarding",
+//   },
+//   {
+//     name: "Jaeyoung",
+//     url: "playing the piano",
+//   },
+// ];
+
+// const usersRoutes = () => {
+//   router.get("/", (req, res) => {
+//     res.json(data);
+//   });
+//   return router;
+// };
+
+// module.exports = usersRoutes;
+//////////
+
 // -- routes/users.js
-const router = require('express').Router();
+const router = require("express").Router();
 
 module.exports = (db) => {
-  // all routes will go here 
-  router.get('/', (req, res) => {
+  // all routes will go here
+  router.get("/", (req, res) => {
     const command = "SELECT * FROM users";
-    db.query(command).then(data => {
+    db.query(command).then((data) => {
       res.json(data.rows);
-    })
+    });
   });
 
   return router;
-}
+};
