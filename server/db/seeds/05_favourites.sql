0b65fae2a19f6a499d8a900d5d02b49a9ac65253
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-03-18 15:25:33 -0400

    added schema and seeds for app

---
+++ server/db/seeds/05_favourites.sql
@@ -0,0 +1,6 @@
+INSERT INTO favourites (mentee_id, mentor_id)
+VALUES
+(4, 1),
+(4, 2),
+(5, 3),
+(6, 2);
---
+++ server/db/seeds/07_mentor_reviews.sql
@@ -0,0 +1,6 @@
+INSERT INTO mentor_reviews (user_id, rating, message) VALUES (1, 5, 'Great mentor! Really helped me figure out what I needed');
+INSERT INTO mentor_reviews (user_id, rating, message) VALUES (1, 4, 'Best mentor in my books');
+INSERT INTO mentor_reviews (user_id, rating, message) VALUES (2, 2, 'Okay mentor at best.');
+INSERT INTO mentor_reviews (user_id, rating, message) VALUES (2, 3, 'Would recommend only if you are a new to coding');
+INSERT INTO mentor_reviews (user_id, rating, message) VALUES (3, 4, 'Makes complex problems fun.');
+INSERT INTO mentor_reviews (user_id, rating, message) VALUES (3, 1, 'Worst experience on this website.');
