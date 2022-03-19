c5c973f8b019b6339c77e3213a83debab1a704cf
Edwin Ly <edwinjly@gmail.com>
2022-03-19 17:09:23 -0400

    Fix session rating post request on client and server

--- client/src/components/Session/Session.js
+++ client/src/components/Session/Session.js
@@ -1,22 +1,29 @@
 import { React, useState, useEffect } from "react";
 // import CardProfile from "./CardProfile";
+import { useNavigate } from "react-router-dom";
+import { Rating } from "@mui/material";
+
 import axios from "axios";
 
 function Session() {
-  const [sessions, setSessions] = useState([]);
+  let navigate = useNavigate();
+  // const [sessions, setSessions] = useState([]);
   // const [message, setMessage] = useState("");
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-  };
+  const [state, setState] = useState({
+    sessions: [],
+    message: "",
+    rating: "",
+  });
 
   useEffect(() => {
     axios
+      //axios url path still hardcoded
       .get("http://localhost:8080/users/1/sessions/1")
       .then((response) => {
         console.log("data!");
         //Need first row of data only
-        setSessions(response.data[0]);
+        setState({ sessions: response.data[0] });
         console.log(response.data);
       })
       .catch((err) => {
@@ -25,34 +32,80 @@ function Session() {
       });
   }, []);
 
+  const onSubmitForm = async (event) => {
+    event.preventDefault();
+    // try {
+    //   // const body = state.message;
+    //   const response = await fetch("http://localhost:8080/users/1/sessions/1", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(state.message),
+    //   });
+    //   console.log(response);
+    // } catch (error) {
+    //   console.error(error.message);
+    // }
+
+    axios
+      .post("http://localhost:8080/users/1/sessions/1", {
+        //hardcoded user_id
+        user_id: 1,
+        message: state.message,
+        rating: state.rating,
+      })
+      .then(function (response) {
+        console.log(response);
+        navigate(`/`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1> Session Page </h1>
       {/* <div> */}
       <aside>
         <span>Thank you</span>
-        <h1>session_id {sessions.id}</h1>
-        <h1>mentor_id {sessions.mentor_id}</h1>
-        <h1>mentee_id {sessions.mentee_id}</h1>
-        <h1>Hi, mentee_name {sessions.mentee_name}</h1>
+        <h1>session_id {state.sessions.id}</h1>
+        <h1>mentor_id {state.sessions.mentor_id}</h1>
+        <h1>mentee_id {state.sessions.mentee_id}</h1>
+        <h1>Hi, mentee_name {state.sessions.mentee_name}</h1>
         <h1>
-          How was your mentor message with mentor_name {sessions.mentor_name}?
+          How was your experience with mentor_name {state.sessions.mentor_name}?
         </h1>
-        <h1>date {sessions.date}</h1>
-        <h1>duration {sessions.duration}</h1>
+        <h1>date {state.sessions.date}</h1>
+        <h1>duration {state.sessions.duration}</h1>
 
-        <form className="form-control">
+        <form className="form-control" onSubmit={onSubmitForm}>
           <label for="message">How was your experience:</label>
           <input
             id="message"
             name="message"
             type="text"
             className="form-control"
-            placeholder="Please write a brief description of how your message went with your mentor"
+            // placeholder="Please write a brief description of how your message went with your mentor"
+            value={state.message}
+            onInput={(event) =>
+              setState({ ...state, message: event.target.value })
+            }
           ></input>
           <label for="rating">Rating ( 0 and 5):</label>
-          <input type="range" id="rating" name="rating" min="0" max="5"></input>
-          <input type="submit" value="Submit"></input>
+
+          <input
+            type="range"
+            id="rating"
+            name="rating"
+            min="0"
+            max="5"
+            value={state.rating}
+            onInput={(event) =>
+              setState({ ...state, rating: event.target.value })
+            }
+          ></input>
+          <button className="btn btn-success">Submit</button>
+          {/* <input type="submit" value="Submit"></input> */}
         </form>
       </aside>
       {/* </div> */}
--- server/routes/users.js
+++ server/routes/users.js
@@ -41,22 +41,32 @@ module.exports = (db) => {
     //works for mentee_name
     // db.query("SELECT sessions.*, users.name as mentee_name FROM sessions JOIN users on users.id = sessions.mentee_id WHERE sessions.id = $1", [user_id]).then((data) => {
 
-    // db.query("SELECT sessions.*, users.name as mentee_name, users.name as mentor_name FROM sessions JOIN users on users.id = sessions.mentee_id JOIN mentors on mentors.id = sessions.mentor_id WHERE users.id = $1 AND sessions.id = $2", [user_id, session_id]).then((data) => {
+    // db.query(`SELECT sessions.*, users.name as mentee_name, users.name as mentor_name
+    // FROM sessions
+    // JOIN users on users.id = sessions.mentee_id
+    // JOIN mentors on mentors.id = sessions.mentor_id W
+    // HERE users.id = $1 AND sessions.id = $2`, [user_id, session_id]).then((data) => {
 
     //works for mentor_name
     db.query(
-      "SELECT sessions.*, users.name as mentor_name FROM sessions JOIN mentors on mentors.id = sessions.mentor_id JOIN users ON users.id = mentors.user_id WHERE users.id = $1 AND sessions.id = $2",
+      `SELECT sessions.*, users.name as mentor_name FROM sessions 
+      JOIN mentors on mentors.id = sessions.mentor_id 
+      JOIN users ON users.id = mentors.user_id 
+      WHERE users.id = $1 
+      AND sessions.id = $2`,
       [user_id, session_id]
     ).then((data) => {
       res.json(data.rows);
     });
   });
 
-  router.post("/:user_id/sessions/:session_id", (req, res) => {
-    const { rating, message } = req.params;
+  router.post("/:user_id/sessions/:session_id", async (req, res) => {
+    const { user_id, session_id } = req.params;
+    const { rating, message } = req.body;
+    console.log(req.body);
     db.query(
-      "INSERT INTO mentor_reviews (rating, message) VALUES ($1, $2) RETURNING *",
-      [rating, message]
+      `INSERT INTO mentor_reviews (user_id, rating, message) VALUES($1, $2, $3)`,
+      [user_id, rating, message]
     ).then((data) => {
       res.json(data.rows);
     });
