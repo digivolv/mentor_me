0bc80c78a31a754dc6bee215a96f36884a159b33
Edwin Ly <edwinjly@gmail.com>
2022-03-19 14:00:57 -0400

    Adjust session routing in client

--- server/routes/users.js
+++ server/routes/users.js
@@ -20,7 +20,7 @@
 //   return router;
 // };
 
-// module.exports = usersRoutes;
+// module.exports = usersRoutes;aaa
 //////////
 
 // -- routes/users.js
@@ -42,21 +42,25 @@ module.exports = (db) => {
     // db.query("SELECT sessions.*, users.name as mentee_name FROM sessions JOIN users on users.id = sessions.mentee_id WHERE sessions.id = $1", [user_id]).then((data) => {
 
     // db.query("SELECT sessions.*, users.name as mentee_name, users.name as mentor_name FROM sessions JOIN users on users.id = sessions.mentee_id JOIN mentors on mentors.id = sessions.mentor_id WHERE users.id = $1 AND sessions.id = $2", [user_id, session_id]).then((data) => {
-    
 
     //works for mentor_name
-    db.query("SELECT sessions.*, users.name as mentor_name FROM sessions JOIN mentors on mentors.id = sessions.mentor_id JOIN users ON users.id = mentors.user_id WHERE users.id = $1 AND sessions.id = $2", [user_id, session_id]).then((data) => {
-      res.json(data.rows)
-    })
-  })
+    db.query(
+      "SELECT sessions.*, users.name as mentor_name FROM sessions JOIN mentors on mentors.id = sessions.mentor_id JOIN users ON users.id = mentors.user_id WHERE users.id = $1 AND sessions.id = $2",
+      [user_id, session_id]
+    ).then((data) => {
+      res.json(data.rows);
+    });
+  });
 
   router.post("/:user_id/sessions/:session_id", (req, res) => {
     const { rating, message } = req.params;
-    db.query("INSERT INTO mentor_reviews (rating, message) VALUES ($1, $2) RETURNING *", [rating, message]).then((data) => {
-      res.json(data.rows)
-    })
-  })
- 
+    db.query(
+      "INSERT INTO mentor_reviews (rating, message) VALUES ($1, $2) RETURNING *",
+      [rating, message]
+    ).then((data) => {
+      res.json(data.rows);
+    });
+  });
 
   return router;
 };
