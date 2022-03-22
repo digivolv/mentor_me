2572c5f95d5f5bc3445ada170a93dab4fe027239
Edwin Ly <edwinjly@gmail.com>
2022-03-22 11:12:49 -0400

    Add put for session page

--- client/src/components/Session/SessionCard.js
+++ client/src/components/Session/SessionCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Rating } from "@mui/material";
+import { Avatar, Grid, Rating, styled } from "@mui/material";
 
 const SessionCard = (props) => {
   const {
@@ -12,17 +12,31 @@ const SessionCard = (props) => {
     duration,
     rating,
     review,
+    picture,
   } = props;
 
   const styles = {
     border: "1px solid rgba(0, 0, 0, 0.05)",
   };
 
+  const Img = styled("img")({
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  });
+
   return (
     <div style={styles}>
       {/* <h3>{`mentor_ID: ${mentor_id}`}</h3> */}
       {/* <h3>{`mentee_id: ${mentee_id}`}</h3> */}
       {/* <h2>{`mentee_name: ${mentee_name}`}</h2> */}
+      <Grid container spacing={1}>
+        <Grid item>
+          {/* <Img src={picture} /> */}
+          <Avatar src={picture} />
+        </Grid>
+      </Grid>
       <Link to={`/mentors/${mentor_id}`}>{`Mentor: ${mentor_name}`}</Link>
       <p>{`Mentorship Date: ${date}`}</p>
       <p>{`Duration: ${duration}`}</p>
