d2144e4a01015eec56af92c7ae537a5c730de0e8
Rosemary Ku <rosemary.ku@hotmail.com>
2022-03-22 09:53:51 -0400

    add rating filter

--- client/src/components/Search/FilterPanel/PriceSlider.js
+++ client/src/components/Search/FilterPanel/PriceSlider.js
@@ -3,14 +3,6 @@ import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 
 function PriceSlider({ value, changePrice }) {
-  // console.log("VALUE:", value);
-  // console.log("CHANGE PRICE:", changePrice);
-  // const [value, setValue] = React.useState([20, 37]);
-
-  // const handleChange = (event, newValue) => {
-  //   setValue(newValue);
-  // };
-
   return (
     <Box sx={{ width: 300 }}>
       <Slider
--- client/src/components/Search/FilterPanel/index.js
+++ client/src/components/Search/FilterPanel/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import PriceSlider from "./PriceSlider";
+import ExperienceCheckbox from "./ExperienceCheckbox";
+import StarRating from "./StarRating";
 
-const FilterPanel = ({ selectedPrice, changePrice }) => {
+const FilterPanel = ({
+  selectedPrice,
+  changePrice,
+  // experiences,
+  // changeExperience,
+}) => {
   return (
     <div>
       <h3>Price Range:</h3>
       <br />
-
       <PriceSlider value={selectedPrice} changePrice={changePrice} />
+      <h3>Years of Experience:</h3>
+      <ExperienceCheckbox />
+      <h3>Rating:</h3>
+      <StarRating />
     </div>
   );
 };
